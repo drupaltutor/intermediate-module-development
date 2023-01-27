5e60ec3bae077163a46db06187a561c9caef07da
Wayne Eaker <wayne@zengenuity.com>
2023-01-27 16:25:30 -0500

    Added form elements to ContactForm

--- form_examples/src/Form/ContactForm.php
+++ form_examples/src/Form/ContactForm.php
@@ -12,7 +12,35 @@ class ContactForm extends FormBase {
   }
 
   public function buildForm(array $form, FormStateInterface $form_state) {
-
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name'),
+    ];
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email Address'),
+      '#required' => TRUE,
+    ];
+    $form['department'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Purpose of Your Message'),
+      '#options' => [
+        'billing' => $this->t('Billing Inquiry'),
+        'tech_support' => $this->t('Technical Support'),
+        'other' => $this->t('Other')
+      ],
+      '#required' => TRUE,
+    ];
+    $form['message'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Message'),
+      '#rows' => 10,
+      '#required' => TRUE,
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
     return $form;
   }
 
@@ -20,6 +48,7 @@ class ContactForm extends FormBase {
     $this->messenger()->addStatus(
       $this->t('Thank you for contacting us. We will respond as soon as possible.')
     );
+    dsm($form_state->getValues());
   }
 
 }
