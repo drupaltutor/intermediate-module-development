402c1c259761580184816942258989716549df75
Wayne Eaker <wayne@zengenuity.com>
2023-01-29 12:35:51 -0500

    Added Hello World block plugin class

---
+++ block_examples/src/Plugin/Block/HelloWorldBlock.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\block_examples\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello World' block.
+ *
+ * @Block(
+ *   id = "block_examples_hello_world",
+ *   admin_label = @Translation("Block Example: Hello World")
+ * )
+ */
+class HelloWorldBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#plain_text' => $this->t('Hello world'),
+    ];
+  }
+}
